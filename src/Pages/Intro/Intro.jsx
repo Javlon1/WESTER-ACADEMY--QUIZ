15f633e3f6f10bf2ed975ac4b41ecda62294bb01
Javlon1 <muhamedmijotot@gmail.com>
2023-11-29 20:21:16 +0300

    end

--- src/Pages/Intro/Intro.jsx
+++ src/Pages/Intro/Intro.jsx
@@ -1,10 +1,65 @@
 import { useState } from 'react';
 import './Intro.scss'
 import xImg from "../../Assets/Img/x.png"
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Intro() {
     const [modal, setModal] = useState(false)
+    const navigate = useNavigate();
 
+    const [formData, setFormData] = useState({
+        name: '',
+        age: '',
+        phoneNumber: '',
+    });
+
+    const [errors, setErrors] = useState({});
+
+    const validateForm = () => {
+        let isValid = true;
+        const newErrors = {};
+
+        const nameRegex = /^[a-zA-Z]+$/;
+        if (!nameRegex.test(formData.name)) {
+            newErrors.name = 'Name should only contain alphabetical characters';
+            isValid = false;
+        }
+
+        const ageValue = parseInt(formData.age, 10);
+        if (isNaN(ageValue) || ageValue < 8 || ageValue > 28) {
+            newErrors.age = 'Age should be a number between 8 and 28';
+            isValid = false;
+        }
+
+        const phoneNumberRegex = /^\d+$/;
+        if (!phoneNumberRegex.test(formData.phoneNumber) || formData.phoneNumber.length !== 9) {
+            newErrors.phoneNumber = 'Invalid phone number (must be a number with length 9)';
+            isValid = false;
+        }
+
+        setErrors(newErrors);
+        return isValid;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (validateForm()) {
+            navigate("/quiz")
+            console.log('Form submitted:', formData);
+        } else {
+
+            console.log('Form validation failed');
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
     return (
         <div className='intro'>
@@ -18,7 +73,7 @@ export default function Intro() {
             </div>
             <div className={modal ? "pop-up-close activeModal" : "pop-up-close"} onClick={() => { setModal(false) }}></div>
             <div className={modal ? "pop-up activeModal" : "pop-up"}>
-                <form className="pop-up__form" action="/quiz">
+                <form onSubmit={handleSubmit} className="pop-up__form">
                     <div className="pop-up__form__top">
                         <h3>IT ga birinchi qadam
                             shu yerdan boshlanadi!</h3>
@@ -28,10 +83,46 @@ export default function Intro() {
                     </div>
                     <p className="pop-up__form__subtitle">Test natijalari bo’yicha yordam berishimiz uchun bizga
                         ma’lumotlaringizni qoldiring.</p>
-                    <input className="pop-up__form__inp" type="text" placeholder="Ismingiz" required />
-                    <input className="pop-up__form__inp" type="number" placeholder="Yoshingiz" required />
-                    <input className="pop-up__form__inp" type="number" placeholder="Telefon" required />
-                    <button className="pop-up__form__btn" type="submit">Yuborish</button>
+                    <div>
+                        <input
+                            className='pop-up__form__inp'
+                            placeholder='Ismingiz'
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                        {errors.name && <p>{errors.name}</p>}
+                    </div>
+
+                    <div>
+                        <input
+                            className='pop-up__form__inp'
+                            placeholder='Yoshingiz'
+                            type="text"
+                            id="age"
+                            name="age"
+                            value={formData.age}
+                            onChange={handleChange}
+                        />
+                        {errors.age && <p>{errors.age}</p>}
+                    </div>
+
+                    <div>
+                        <input
+                            className='pop-up__form__inp'
+                            placeholder='Telefon'
+                            type="text"
+                            id="phoneNumber"
+                            name="phoneNumber"
+                            value={formData.phoneNumber}
+                            onChange={handleChange}
+                        />
+                        {errors.phoneNumber && <p>{errors.phoneNumber}</p>}
+                    </div>
+
+                    <button className='pop-up__form__btn' type="submit">Submit</button>
                 </form>
             </div>
         </div>
